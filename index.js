bf17b2585562727abdd2e111886aac2bfb350afd
sauravsaran99 <sauravsaran99@gmail.com>
2023-10-19 03:22:55 +0530

    bux fixing

--- index.js
+++ index.js
@@ -7,28 +7,18 @@ const notesRoutes = require("./src/routes/noteRoutes");
 const connectDB = require("./src/config/connectDb");
 
 const app = express();
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://smalljoys-frontend.vercel.app",
-      "https://smalljoys-frontend.vercel.app/login",
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
 connectDB();
 
 // Routes
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// })
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/notes", notesRoutes);
 // app.use('/notes', noteRoutes);
