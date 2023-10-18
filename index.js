2328756484d72994b513dce52ef163216ccb9130
sauravsaran99 <sauravsaran99@gmail.com>
2023-10-19 04:51:12 +0530

    update

--- index.js
+++ index.js
@@ -7,47 +7,19 @@ const notesRoutes = require("./src/routes/noteRoutes");
 const connectDB = require("./src/config/connectDb");
 
 const app = express();
-
-const options = [
+app.use(
   cors({
-    origin: "*",
-    methods: "*",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-];
-
-app.use(options);
-
+    origin: ["http://localhost:3001", "https://smalljoys-frontend.vercel.app", "https://smalljoys-frontend.vercel.app/", "https://smalljoys-frontend.vercel.app/login"],
+     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, 
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
 connectDB();
 
 // Routes
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "https://smalljoys-frontend.vercel.app/");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/notes", notesRoutes);
 // app.use('/notes', noteRoutes);
