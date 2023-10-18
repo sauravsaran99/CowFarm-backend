c2cd7ad869b68fb303e594e6698ac39da0d58588
sauravsaran99 <sauravsaran99@gmail.com>
2023-10-19 03:07:57 +0530

    new update

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.use(express.json());
 connectDB();
 
 // Routes
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/notes", notesRoutes);
 // app.use('/notes', noteRoutes);
